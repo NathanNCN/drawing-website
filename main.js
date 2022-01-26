dd923faa2a936f9b5327ff1ea63a3a61bf44d52c
NathanNCN <78819516+NathanNCN@users.noreply.github.com>
2022-01-25 22:21:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let tablet = document.getElementById("gridDom"); // getting elements from page
 let bttnTools = document.getElementById('tools');
 let bttnGrid = document.getElementById('grid');
+let bttnEraser = document.getElementById('eraser');
+let bttnColor = document.getElementById('color')
+let bttnBlack = document.getElementById('black')
 let containerTools = document.getElementById('ContainerTools');
 let containerGrid = document.getElementById('ContainerGrid');
 let sliderGrid = document.getElementById('slider');
 let text = document.getElementById('textBox');
+let currentTrail = 'black';
+
 
 
 function restGrid(){ // rests the grid
@@ -16,11 +21,11 @@ function restGrid(){ // rests the grid
 
 function gridSize(size){ //updates the tablet to the users grid size
     restGrid();
-    text.textContent = sliderGrid.value;
+    text.textContent = sliderGrid.value + ' x ' + sliderGrid.value;
     tablet.style.gridTemplateColumns = 'repeat('+ Math.sqrt(size) + ', 1fr)'
     for (let i=0; i<size; i++){
         let div = document.createElement('div');
-        div.classList.add('grid-cell');//remove after only meant for testing
+        div.addEventListener('mouseover', trail);
         tablet.appendChild(div);
     }
 }
@@ -35,15 +40,25 @@ function showGrid(){ //shows slider which controls grid size
     containerGrid.style.display = 'flex';
 }
 
+function trail(event){
+    event.target.classList.add('grid-trail')
+}
+
+
 containerTools.style.display = 'none';//makes it so you see grid first
 
-gridSize(16*16);
+gridSize(16*16);// make the defualt grid 16x16
+
 
 
 bttnTools.addEventListener('click', () => showTools());
 bttnGrid.addEventListener('click', () => showGrid());
 
+bttnEraser.addEventListener('click',{
+    
+})
+
 
 sliderGrid.addEventListener('mouseup', () => gridSize(sliderGrid.value*sliderGrid.value));
 
-
+gridCell.addEventListener('mouseover', () => trail());
